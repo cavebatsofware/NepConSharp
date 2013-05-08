115e7d78668eacaaab533780933cd6876c12b8e5
Grant Defayette <grant.defayette@thenortheastgroup.com>
2013-05-08 12:01:15 -0400

    Sysinfo changes

--- NepConSharp/MainWindow.cs
+++ NepConSharp/MainWindow.cs
@@ -5,6 +5,8 @@ using NepConSharp;
 public partial class MainWindow: Gtk.Window
 {	
 	private Gtk.ListStore systemListStore;
+	private SystemInformation selectedSystem;
+	private static SystemInfoDialog systemInfoDialog;
 
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
@@ -35,13 +37,45 @@ public partial class MainWindow: Gtk.Window
 		lstSystem.AppendColumn (hostCol);
 		systemListStore = new Gtk.ListStore (typeof(string), typeof(string));
 		lstSystem.Model = systemListStore;
+		lstSystem.Selection.Changed += OnSelectionChanged;
+		SystemInformation.ItemSaved += OnItemSaved;
 		foreach (SystemInformation system in SystemInformation.GetAll())
 			systemListStore.AppendValues (system.name, system.host);
 	}
+
+	protected void OnItemSaved (object sender, EventArgs e)
+	{
+		var system = (SystemInformation) sender;
+		systemListStore.AppendValues (system.name, system.host);
+	}
+
+	protected void OnConnectClicked (object sender, EventArgs e)
+	{
+		Console.WriteLine ("CONNECT: ");
+	}
+
+	protected void OnNewClicked (object sender, EventArgs e)
+	{
+		var system = selectedSystem == null ? new SystemInformation () : selectedSystem;
+		systemInfoDialog = new SystemInfoDialog(system);
+		systemInfoDialog.Show();
+	}
+
+    protected void OnSelectionChanged (object o, EventArgs args)
+    {
+        TreeIter iter;
+        TreeModel model;
+        if (((TreeSelection)o).GetSelected (out model, out iter))
+        {
+            var value = (string) model.GetValue (iter, 0);
+			selectedSystem = SystemInformation.FindByName (value);
+        }
+    }
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
 		a.RetVal = true;
 	}
+
 }
--- NepConSharp/gtk-gui/MainWindow.cs
+++ NepConSharp/gtk-gui/MainWindow.cs
@@ -3,17 +3,71 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.UIManager UIManager;
+	private global::Gtk.Action FileAction;
+	private global::Gtk.Action LoadSettingsFileAction;
+	private global::Gtk.Action connectAction;
+	private global::Gtk.Action disconnectAction;
+	private global::Gtk.Action newAction;
+	private global::Gtk.Action openAction;
+	private global::Gtk.VBox vbox1;
+	private global::Gtk.MenuBar menubar1;
+	private global::Gtk.Toolbar toolbar1;
 	private global::Gtk.ScrolledWindow GtkScrolledWindow;
 	private global::Gtk.TreeView lstSystem;
+	private global::Gtk.Statusbar barStatus;
+	private global::Gtk.Label lblUser;
+	private global::Gtk.Label lblConnInfo;
 	
 	protected virtual void Build ()
 	{
 		global::Stetic.Gui.Initialize (this);
 		// Widget MainWindow
+		this.UIManager = new global::Gtk.UIManager ();
+		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup ("Default");
+		this.FileAction = new global::Gtk.Action ("FileAction", global::Mono.Unix.Catalog.GetString ("File"), null, null);
+		this.FileAction.ShortLabel = global::Mono.Unix.Catalog.GetString ("File");
+		w1.Add (this.FileAction, null);
+		this.LoadSettingsFileAction = new global::Gtk.Action ("LoadSettingsFileAction", global::Mono.Unix.Catalog.GetString ("Load settings file"), null, null);
+		this.LoadSettingsFileAction.ShortLabel = global::Mono.Unix.Catalog.GetString ("Load settings file");
+		w1.Add (this.LoadSettingsFileAction, null);
+		this.connectAction = new global::Gtk.Action ("connectAction", null, null, "gtk-connect");
+		w1.Add (this.connectAction, null);
+		this.disconnectAction = new global::Gtk.Action ("disconnectAction", null, null, "gtk-disconnect");
+		w1.Add (this.disconnectAction, null);
+		this.newAction = new global::Gtk.Action ("newAction", null, null, "gtk-new");
+		w1.Add (this.newAction, null);
+		this.openAction = new global::Gtk.Action ("openAction", null, null, "gtk-open");
+		w1.Add (this.openAction, null);
+		this.UIManager.InsertActionGroup (w1, 0);
+		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
 		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.vbox1 = new global::Gtk.VBox ();
+		this.vbox1.Name = "vbox1";
+		this.vbox1.Spacing = 6;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.UIManager.AddUiFromString ("<ui><menubar name='menubar1'><menu name='FileAction' action='FileAction'><menuitem name='LoadSettingsFileAction' action='LoadSettingsFileAction'/></menu></menubar></ui>");
+		this.menubar1 = ((global::Gtk.MenuBar)(this.UIManager.GetWidget ("/menubar1")));
+		this.menubar1.Name = "menubar1";
+		this.vbox1.Add (this.menubar1);
+		global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.menubar1]));
+		w2.Position = 0;
+		w2.Expand = false;
+		w2.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar1'><toolitem name='connectAction' action='connectAction'/><toolitem name='disconnectAction' action='disconnectAction'/><toolitem name='newAction' action='newAction'/><toolitem name='openAction' action='openAction'/></toolbar></ui>");
+		this.toolbar1 = ((global::Gtk.Toolbar)(this.UIManager.GetWidget ("/toolbar1")));
+		this.toolbar1.Name = "toolbar1";
+		this.toolbar1.ShowArrow = false;
+		this.vbox1.Add (this.toolbar1);
+		global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.toolbar1]));
+		w3.Position = 1;
+		w3.Expand = false;
+		w3.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
 		this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
 		this.GtkScrolledWindow.Name = "GtkScrolledWindow";
 		this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
@@ -22,7 +76,35 @@ public partial class MainWindow
 		this.lstSystem.CanFocus = true;
 		this.lstSystem.Name = "lstSystem";
 		this.GtkScrolledWindow.Add (this.lstSystem);
-		this.Add (this.GtkScrolledWindow);
+		this.vbox1.Add (this.GtkScrolledWindow);
+		global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.GtkScrolledWindow]));
+		w5.Position = 2;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.barStatus = new global::Gtk.Statusbar ();
+		this.barStatus.Name = "barStatus";
+		this.barStatus.Spacing = 6;
+		// Container child barStatus.Gtk.Box+BoxChild
+		this.lblUser = new global::Gtk.Label ();
+		this.lblUser.Name = "lblUser";
+		this.barStatus.Add (this.lblUser);
+		global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.barStatus [this.lblUser]));
+		w6.Position = 1;
+		w6.Expand = false;
+		w6.Fill = false;
+		// Container child barStatus.Gtk.Box+BoxChild
+		this.lblConnInfo = new global::Gtk.Label ();
+		this.lblConnInfo.Name = "lblConnInfo";
+		this.barStatus.Add (this.lblConnInfo);
+		global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.barStatus [this.lblConnInfo]));
+		w7.Position = 2;
+		w7.Expand = false;
+		w7.Fill = false;
+		this.vbox1.Add (this.barStatus);
+		global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.barStatus]));
+		w8.Position = 3;
+		w8.Expand = false;
+		w8.Fill = false;
+		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
@@ -30,5 +112,7 @@ public partial class MainWindow
 		this.DefaultHeight = 300;
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
+		this.connectAction.Activated += new global::System.EventHandler (this.OnConnectClicked);
+		this.newAction.Activated += new global::System.EventHandler (this.OnNewClicked);
 	}
 }
