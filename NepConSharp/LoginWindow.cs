10dabdb929466138226bc8f98481f0f4b6a6fafa
Grant Defayette <grant.defayette@thenortheastgroup.com>
2013-06-04 09:13:19 -0400

    Use event to notify login.

--- NepConSharp/LoginWindow.cs
+++ NepConSharp/LoginWindow.cs
@@ -3,8 +3,22 @@ using Gtk;
 
 namespace NepConSharp
 {
+	public class LoginEventArgs : EventArgs
+	{
+		public string Username = null;
+		public string Password = null;
+		public LoginEventArgs (string _username, string _password)
+		{
+			this.Username = _username;
+			this.Password = _password;
+		}
+	}
+
 	public partial class LoginWindow : Gtk.Window
 	{
+		public delegate void LoginClickedHandler (object sender, LoginEventArgs data);
+		public event LoginClickedHandler LoginClicked;
+
 		public LoginWindow () : 
 				base(Gtk.WindowType.Toplevel)
 		{
@@ -15,7 +29,11 @@ namespace NepConSharp
 		{
 			string name = tbxName.Text;
 			string password = tbxPassword.Text;
-			MainClass.BeginLogin (name, password);
+			if (this.LoginClicked != null) {
+				var args = new LoginEventArgs (name, password);
+				LoginClicked (this, args);
+			}
+			this.Hide ();
 		}
 
 		protected void OnDeleteEvent (object sender, DeleteEventArgs a)
