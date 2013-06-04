a3ae88181b99b7f0bef182b789898fb25ef08728
Grant Defayette <grant.defayette@thenortheastgroup.com>
2013-06-04 09:10:01 -0400

    Semi final sysinfo dialog.

--- NepConSharp/SystemInfoDialog.cs
+++ NepConSharp/SystemInfoDialog.cs
@@ -4,25 +4,45 @@ namespace NepConSharp
 {
 	public partial class SystemInfoDialog : Gtk.Dialog
 	{
-		public SystemInformation Info;
+		public RemoteSystem Info;
+		public SystemList SystemList;
 
-		public SystemInfoDialog (SystemInformation info)
+		public SystemInfoDialog (RemoteSystem _info, SystemList _list): base ()		
 		{
+			Info = _info;
+			SystemList = _list;
 			this.Build ();
-			this.LoadGateways();
-			this.LoadProtocols();
-			Info = info;
+			this.LoadGateways ();
+			this.LoadProtocols ();
+			this.Populate ();
 		}
 
-		public void LoadGateways()
+		public void Populate ()
 		{
-			var gatewayInfos = SystemInformation.GetAllGW();
-			foreach(SystemInformation gateway in gatewayInfos) {
-				this.gateway.AppendText(gateway.name);
+			this.entryName.Text = Info.GetName ();
+			this.userName.Text = Info.GetUser ();
+			this.port.Text = Info.GetPort ();
+			this.host.Text = Info.GetHost ();
+			if (Info.GetIsGateway () == "true") this.isGateway.Active = true;
+			if (Info.GetAuthType () == "password") {
+				this.password.Text = Info.GetAuthKey ();
+				this.authPassword.Active = true;
+			} else {
+				this.authPublicKey.Active = true;
+			}
+			SetSelectedItem (this.gateway, Info.GetGatewayName ());
+			SetSelectedItem (this.protocol, Info.GetProtocol ());
+		}
+
+		public void LoadGateways ()
+		{
+			var gatewayInfos = SystemList.FindAllGateways ();
+			foreach(RemoteSystem gateway in gatewayInfos) {
+				this.gateway.AppendText (gateway.GetName ());
 			}
 		}
 
-		public void LoadProtocols()
+		public void LoadProtocols ()
 		{
 			//TODO: Protocols should be dynamically loaded here instead
 			//      of hardcoded. Each protocol should probably be it's
@@ -37,22 +57,32 @@ namespace NepConSharp
 			return "";
 		}
 
+		private void SetSelectedItem (Gtk.ComboBox box, string _value)
+		{
+			Gtk.TreeIter iter;
+			box.Model.GetIterFirst (out iter);
+			do {
+				var row = (string) box.Model.GetValue (iter, 0);
+				if (String.Equals (_value, row)) {
+					box.SetActiveIter (iter);
+					break;
+				}
+			} while (box.Model.IterNext (ref iter));
+		}
+
 		protected void OnApplyClickedEvent (object sender, EventArgs e)
 		{
-			Console.WriteLine (this.entryName.Text);
-			Console.WriteLine (this.userName.Text);
-			Console.WriteLine (this.password.Text);
-
-			Info.name = this.entryName.Text;
-			Info.host = this.host.Text;
-			Info.isgw = this.isGateway.Active ? "true" : "false";
-			Info.hasgw = this.GetSelectedItem (this.gateway);
-			Info.authtype = this.authPublicKey.Active ? "key" : "password";
-			Info.authkey = this.password.Text;
-			Info.port = this.port.Text;
-			Info.protocol = this.GetSelectedItem (this.protocol);
-			Info.user = this.userName.Text;
-			Info.Save ();
+			Info.SetName (this.entryName.Text);
+			Info.SetHost (this.host.Text);
+			Info.SetIsGateway (this.isGateway.Active ? "true" : "false");
+			Info.SetGatewayName (this.GetSelectedItem (this.gateway));
+			Info.SetAuthType (this.authPublicKey.Active ? "key" : "password");
+			Info.SetAuthKey (this.password.Text);
+			Info.SetPort (this.port.Text);
+			Info.SetProtocol (this.GetSelectedItem (this.protocol));
+			Info.SetUser (this.userName.Text);
+			SystemList.Add (Info);
+			this.Destroy ();
 			//TODO: Return attributes and/or save system info.
 		}
 				
